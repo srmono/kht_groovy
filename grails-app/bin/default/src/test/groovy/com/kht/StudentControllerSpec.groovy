7927a9ebd66426e83b9999cf411cce2f5e1cfebd
srmono <bvsrao91@gmail.com>
2022-11-09 07:20:40 +0530

    grails update

--- grails-app/bin/default/src/test/groovy/com/kht/StudentControllerSpec.groovy
+++ grails-app/bin/default/src/test/groovy/com/kht/StudentControllerSpec.groovy
@@ -11,6 +11,22 @@ class StudentControllerSpec extends Specification implements ControllerUnitTest<
     def cleanup() {
     }
 
+    void "Test the index action returns the correct model" (){
+        given:
+        controller.studentService = Mock(StudentService) {
+            list() >> [new Student(firstName: "venkat", lastName: "bvs")]
+        }
+
+        when: "The index action is executed"
+        controller.index()
+
+        then: "The model is correct"
+        model.studentList.size() == 1
+        model.studentList[0].firstName = "venkat"
+        model.studentList[0].lastName = "bvs"
+
+    }
+
     void "test something"() {
         expect:"fix me"
             true == false
